ae0f2e577caf63d0705a515a74657479a9c93383
alexander-levitsky <piro@list.ru>
2020-01-07 09:57:04 +0300

    Init

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let template= `
+<div>
+    <h1>Компонент 2</h1>
+</div>
+`;
+
+export default {
+    template: template,
+    data() {
+        return {}
+    },
+    mounted() {
+        console.log('index mounted');
+    }
+}
